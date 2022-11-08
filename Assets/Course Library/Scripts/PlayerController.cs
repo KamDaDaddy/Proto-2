d0616934fb261cad5275d4ded2b7fd1c60908528
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2022-11-08 11:19:10 -0500

    ...

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //private
     private float horizontalInput;
+    private bool gameOver = false; 
+    //public
     public float speed = 10.0f;
     public GameObject projectilePrefab;
     public float xRange = 10.0f;
@@ -27,9 +30,11 @@ public class PlayerController : MonoBehaviour
         {   
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+        //gets horizontal key input
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+        //if space is pressed, player "jumps".
         if(Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
